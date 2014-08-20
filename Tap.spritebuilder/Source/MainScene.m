454a210cb634e7ef6c1c916793a77e112caa60b8
Shin Uesugi <shinsaku417@gmail.com>
2014-08-20 11:50:52 -0700

    mainscene

--- Tap.spritebuilder/Source/MainScene.m
+++ Tap.spritebuilder/Source/MainScene.m
@@ -8,7 +8,28 @@
 
 #import "MainScene.h"
 
-@implementation MainScene
+@implementation MainScene {
+    CCLabelTTF *_tap;
+    CCLabelTTF *_pro;
+    CCButton *_playButton;
+}
+
+- (void)onEnter {
+    [super onEnter];
+    
+    [self bounce:_tap];
+    [self bounce:_pro];
+    
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(_playButton.position.x, 0.5)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [_playButton runAction:bounceOut];
+}
+
+- (void)bounce:(CCSprite *)sprite {
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(0.5, sprite.position.y)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [sprite runAction:bounceOut];
+}
 
 - (void)play {
     CCScene *gameplayScene = [CCBReader loadAsScene:@"GamePlay"];
